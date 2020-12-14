307dd4874c8f752600348802f8d8bfdd538af620
Jimmy Hui <jimmyhui92012@gmail.com>
2020-12-14 10:09:31 -0800

    Added pyperclip

--- webscraping/mapit.py
+++ webscraping/mapit.py
@@ -2,11 +2,20 @@
 # browser using an address from the command line
 
 import webbrowser, sys
+import pyperclip
 
+
+prefix = "https://www.google.com/maps/place/"
+# If there are arguments, use args as address
+# If there are no arguments, grab from clipboard
 if len(sys.argv) >1:
     address = " ".join(sys.argv[1:])
-    prefix = "https://www.google.com/maps/place/"
-
     webbrowser.open(prefix + address)
 
+else:
+    address = pyperclip.paste()
+    webbrowser.open(prefix + address)
+
+
 # TODO: Get the address from the clipboard
+# If there are no arguments, grab from clipboard
